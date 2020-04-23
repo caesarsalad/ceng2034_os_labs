f0df1ea019b742af9be571191080340ee6a17842
cakirhuseyin <ckrhuseyin48@gmail.com>
2020-04-23 19:56:15 +0300

    fix folders

--- lab8/copyScript.py
+++
@@ -1,6 +0,0 @@
-import shutil, random
-
-for i in range (10,150):
-    name = "{0}{1}".format("file", i)
-    origin = "{0}{1}".format("file", random.randint(1,4))
-    shutil.copyfile(origin, name )
